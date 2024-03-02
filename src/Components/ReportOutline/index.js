b9effa0a9f4a93c8fbc611e907ceb24d57db0052
Duc Anh <161408884+INTS-DucAnh@users.noreply.github.com>
2024-03-02 13:13:29 +0700

    Update index.js

--- src/Components/ReportOutline/index.js
+++ src/Components/ReportOutline/index.js
@@ -1,10 +1,10 @@
 export default function ReportOutline({ isOpen, data }) {
   if (!isOpen) return null;
 
-  const TagName = ({ element }) => {
+  const TagName = ({ element, ...props }) => {
     const Tag = element.tagName;
     return (
-      <a href={`#${element.id}`} className="scroll-mt-32">
+      <a {...props} href={`#${element.id}`} className="scroll-mt-32">
         <Tag
           style={{
             marginLeft: `${parseInt(element.tagName.slice(-1) - 1) * 10}px`,
@@ -18,7 +18,7 @@ export default function ReportOutline({ isOpen, data }) {
   return (
     <div className="w-full mt-5 2xl:w-fit 2xl:bg-white 2xl:bg-opacity-5 2xl:rounded-xl 2xl:border-1 2xl:backdrop-blur-sm 2xl:border-stone-600 2xl:absolute 2xl:left-0 2xl:right-0 2xl:box-border p-3">
       {Object.values(data).map((e) => (
-        <TagName element={e} />
+        <TagName element={e} key={e.id}/>
       ))}
     </div>
   );
