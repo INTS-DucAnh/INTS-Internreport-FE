b20c95d09d185ce4cf4d4d52be01a77e998a9148
DucAnh2611 <ducanhmc24@gmail.com>
2024-03-06 15:48:19 +0700

    Reformat outline of report

--- src/Components/ReportOutline/index.js
+++ src/Components/ReportOutline/index.js
@@ -8,7 +8,7 @@ export default function ReportOutline({ isOpen, data, ...props }) {
     return (
       <a
         href={`#${element.id}`}
-        className="opacity-50 hover:opacity-100 relative before:hidden hover:before:block before:absolute before:left-0 before:top-0 before:h-full before:bg-gradient-to-r before:from-[#bb6dff75] before:to-transparent before:border-l-3 before:border-purple-200 before:to-[20%] before:w-full"
+        className=" opacity-50 hover:opacity-100 relative before:hidden hover:before:block before:absolute before:left-0 before:top-0 before:h-full before:bg-gradient-to-r before:from-[#bb6dff75] before:to-transparent before:border-l-3 before:border-purple-200 before:to-[20%] before:w-full"
       >
         <Tag>{element.text}</Tag>
       </a>
@@ -17,7 +17,7 @@ export default function ReportOutline({ isOpen, data, ...props }) {
   return (
     <div
       {...props}
-      className="w-full m-5 2xl:w-fit 2xl:max-w-[35%]: mdxl:max-w-[60%] 2xl:bg-white 2xl:bg-opacity-5 2xl:rounded-xl 2xl:border-1 2xl:backdrop-blur-sm 2xl:border-stone-600 2xl:absolute 2xl:left-0 2xl:right-0 2xl:box-border p-5"
+      className="w-full m-5 2xl:w-fit 2xl:max-w-[35%]: xl:max-w-[60%] 2xl:bg-white 2xl:bg-opacity-5 2xl:rounded-xl 2xl:border-1 2xl:backdrop-blur-sm 2xl:border-stone-600 2xl:absolute 2xl:left-0 2xl:right-0 2xl:box-border p-5"
     >
       <p className=" text-2xl font-bold">Outline</p>
       <Divider orientation="horizontal" className="my-3" />
